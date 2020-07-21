69b813bc8976f6cd8962df882b45c6a940e83357
gamorosino <gamorosino@gmail.com>
2020-07-21 18:47:15 +0200

    minor changes

--- COVIDNet_inference.sh
+++ COVIDNet_inference.sh
@@ -1,17 +1,33 @@
+#! /bin/bash
+
+##################################################################################################################
+### Input parsing
+##################################################################################################################
+
 input_path=$1
 COVIDNet_model=$2
 output_txt=$3
 
 
 		if [ $# -lt 1 ]; then							# usage dello script							
-			    echo $0: "usage: COVIDNet_inference.sh <input_path> [<COVIDNet_model>] [<output_txt>]"
+			    echo $0: "usage: COVIDNet_inference.sh <input_path> [<COVIDNet_model>] [<output_txt>]"			    
+			    echo     "COVIDNet_model:   "
+			    echo     "                1. COVIDNet-CXR3-A"
+			    echo     "                2. COVIDNet-CXR3-B"
+			    echo     "                3. COVIDNet-CXR3-C"
+			    echo     "                4. COVIDNet-CXR-Large"
+			    echo     "                5. COVIDNet-CXR-Small"	
 			    exit 1;		    
 		fi   
+
+##################################################################################################################
+### Functions
+##################################################################################################################
 
 str_index() {    
                 
                 ############# ############# ############# ############# ############# ############# 
-                ############  Trova il primo indice di una sottostringa in una stringa  ########### 
+                ############        Find the first index of a substring in a string     ########### 
                 ############# ############# ############# ############# ############# #############   
 
 		if [ $# -lt 2 ]; then							# usage dello script							
@@ -25,12 +41,16 @@ str_index() {
 
 fbasename () {
                 ############# ############# ############# ############# ############# ############# 
-                #############   Rimuone directory ed estenzione dal nome di un file   ############# 
+                #############      Remove directory and extension from a file name    ############# 
                 ############# ############# ############# ############# ############# #############
                   
                 echo ` basename $1 | cut -d '.' -f 1 `
 		
 		};
+
+##################################################################################################################
+### main
+##################################################################################################################
 
 SCRIPT=`realpath -s $0`
 dir_script=`dirname $SCRIPT`
@@ -44,19 +64,43 @@ model_v=( $( ls ${weightspath}"/model-"* )  )
 model_str=${model_v[0]}
 sind=$( str_index ${model_v[0]} "." )
 ckptname=$( basename ${model_str:0:${sind}} )
-[ -z ${output_txt} ] && { output_txt=$( dirname ${input_path} )'/'$( fbasename ${input_path} )"_"${COVIDNet_model}".txt"; }
+[ -z ${output_txt} ] && \
+	{ output_txt=$( dirname ${input_path} )'/'$( fbasename ${input_path} )"_"${COVIDNet_model}".txt"; }
 
-( [ "${COVIDNet_model}" ==  "COVIDNet-CXR-Large"  ] || [  "${COVIDNet_model}" ==  "COVIDNet-CXR-Small" ] ) && { ocommands=" --out_tensor dense_3/Softmax:0 --input_size 224 " ; }
+( [ "${COVIDNet_model}" ==  "COVIDNet-CXR-Large"  ] || \
+	[  "${COVIDNet_model}" ==  "COVIDNet-CXR-Small" ] ) &&\
+	 { ocommands=" --out_tensor dense_3/Softmax:0 --input_size 224 " ; }
 
 
 printf "" > ${output_txt}
 
 if [ -d ${input_path} ]; then
 	temp_txt=${input_path}"/COVIDNet_inference"$( date +%s )".txt"
-	for i in $( ls ${input_path} ); do  printf "Image: "${i} ; echo "Image: "${i} >> ${temp_txt} ; python ${COVIDNet_DIR}"/inference.py"     --weightspath ${weightspath}     --metaname "model.meta"     --ckptname $ckptname    --imagepath ${input_path}"/"${i} ${ocommands} 1>> ${temp_txt} 2>> /dev/null  ; cat ${temp_txt} | grep "Image" >> ${output_txt};  cat ${temp_txt} | grep "Normal" >> ${output_txt}; prediction=$( cat ${temp_txt} | grep   "Prediction" ) ; echo ${prediction} >> ${output_txt} ; echo " - "${prediction}  ;rm ${temp_txt}; done
+	for i in $( ls ${input_path} ); do  printf "Image: "${i} ; echo "Image: "${i} >> ${temp_txt} ; 
+		python ${COVIDNet_DIR}"/inference.py"     \
+						  --weightspath ${weightspath}     \
+						  --metaname "model.meta"     \
+						  --ckptname $ckptname    \
+						  --imagepath ${input_path}"/"${i} ${ocommands} \
+						  1>> ${temp_txt} 2>> /dev/null  ; \
+						  cat ${temp_txt} | grep "Image" >> ${output_txt};  \
+						  cat ${temp_txt} | grep "Normal" >> ${output_txt}; \
+						  prediction=$( cat ${temp_txt} | grep   "Prediction" ) ; \
+						  echo ${prediction} >> ${output_txt} ; echo " - "${prediction};  \
+						  rm ${temp_txt}; 
+	done
 
 else
 	temp_txt=$( dirname ${input_path} )"/COVIDNet_inference"$( date +%s )".txt"
-	python ${COVIDNet_DIR}"/inference.py"     --weightspath ${weightspath}     --metaname "model.meta"     --ckptname $ckptname    --imagepath ${input_path} ${ocommands} 1>> ${temp_txt}  2>> /dev/null   ; cat ${temp_txt} | grep "Image" >> ${output_txt};  cat ${temp_txt} | grep "Normal" >> ${output_txt}; prediction=$( cat ${temp_txt} | grep   "Prediction" ) ; echo ${prediction} >> ${output_txt} ; echo " - "${prediction}  ;rm ${temp_txt};
+	python ${COVIDNet_DIR}"/inference.py"     --weightspath ${weightspath}     \
+						  --metaname "model.meta"     \
+						  --ckptname $ckptname    \
+						  --imagepath ${input_path} ${ocommands} \
+						  1>> ${temp_txt}  2>> /dev/null   ; \
+						  cat ${temp_txt} | grep "Image" >> ${output_txt};  \
+						  cat ${temp_txt} | grep "Normal" >> ${output_txt}; \
+						  prediction=$( cat ${temp_txt} | grep   "Prediction" ) ; \
+						  echo ${prediction} >> ${output_txt} ; echo " - "${prediction};  \
+						  rm ${temp_txt};
 
 fi
